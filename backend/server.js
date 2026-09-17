63a229a1448c15851c5a16f1b5cc9bc0fa029348
Mathumith M <methuleshmari@gmail.com>
2026-09-17 16:03:03 +0530

    Use ES module imports in server

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
-const taskroutes=require("./src/routes/taskRoute.js");
-const express=require("express");
-const cors=require("cors")
+import taskroutes from "./src/routes/taskRoute.js";
+import express from "express";
+import cors from "cors";
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(taskroutes);;
+app.use(taskroutes);
 app.listen(3000,function(){
     console.log("server is running on port 3000");
-})
+});
